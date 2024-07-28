5c7cdc76419b3b855d156a247d9ce0f0e0a523e5
Rekuiem84 <morinijoshua@gmail.com>
2024-07-28 04:25:52 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let cont = document.querySelector(".cont");
+
+const number_of_items_per_turn = 5;
+const total_number_of_items = 12;
+const num_of_cards_ratio = total_number_of_items / number_of_items_per_turn;
+
+gsap.to(cont, {
+	rotateY: -360 * num_of_cards_ratio + 360 / number_of_items_per_turn,
+	ease: "linear",
+	scrollTrigger: {
+		trigger: cont,
+		start: "top 40%",
+		end: "bottom 80%",
+		scrub: true,
+	},
+});
